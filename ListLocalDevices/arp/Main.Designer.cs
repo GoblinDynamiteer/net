b25e0b07861fc9d6632917b025daa3c56d3a46b1
Johan Kämpe <kampe.johan@gmail.com>
2017-09-25 14:44:28 +0200

    Use arp -a to list devices on network, filter out IP/MAC

---
+++ ListLocalDevices/arp/Main.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace arp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxDevices = new System.Windows.Forms.ListBox();
+            this.btnGetDevices = new System.Windows.Forms.Button();
+            this.tbIp = new System.Windows.Forms.TextBox();
+            this.tbMac = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBoxDevices
+            // 
+            this.listBoxDevices.FormattingEnabled = true;
+            this.listBoxDevices.ItemHeight = 25;
+            this.listBoxDevices.Location = new System.Drawing.Point(26, 83);
+            this.listBoxDevices.Name = "listBoxDevices";
+            this.listBoxDevices.Size = new System.Drawing.Size(803, 504);
+            this.listBoxDevices.TabIndex = 0;
+            this.listBoxDevices.SelectedIndexChanged += new System.EventHandler(this.listBoxDevices_SelectedIndexChanged);
+            // 
+            // btnGetDevices
+            // 
+            this.btnGetDevices.Location = new System.Drawing.Point(26, 605);
+            this.btnGetDevices.Name = "btnGetDevices";
+            this.btnGetDevices.Size = new System.Drawing.Size(233, 76);
+            this.btnGetDevices.TabIndex = 1;
+            this.btnGetDevices.Text = "Get Devices";
+            this.btnGetDevices.UseVisualStyleBackColor = true;
+            this.btnGetDevices.Click += new System.EventHandler(this.btnGetDevices_Click);
+            // 
+            // tbIp
+            // 
+            this.tbIp.Location = new System.Drawing.Point(26, 726);
+            this.tbIp.Name = "tbIp";
+            this.tbIp.Size = new System.Drawing.Size(233, 31);
+            this.tbIp.TabIndex = 2;
+            // 
+            // tbMac
+            // 
+            this.tbMac.Location = new System.Drawing.Point(298, 726);
+            this.tbMac.Name = "tbMac";
+            this.tbMac.Size = new System.Drawing.Size(233, 31);
+            this.tbMac.TabIndex = 3;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 884);
+            this.Controls.Add(this.tbMac);
+            this.Controls.Add(this.tbIp);
+            this.Controls.Add(this.btnGetDevices);
+            this.Controls.Add(this.listBoxDevices);
+            this.Name = "Main";
+            this.Text = "ARP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxDevices;
+        private System.Windows.Forms.Button btnGetDevices;
+        private System.Windows.Forms.TextBox tbIp;
+        private System.Windows.Forms.TextBox tbMac;
+    }
+}
+
---
+++ ListLocalDevices/arp/Main.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace arp
+{
+    public partial class Main : Form
+    {
+        string regexIp = @"\d+\.\d+\.\d+\.\d+";
+        // string regexMac = @"([0 - 9a - f]{2}\-){5}[0-9a-f]{2}";
+        string regexMac = @".{2}\-.{2}\-.{2}\-.{2}\-.{2}\-.{2}";
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetDevices_Click(object sender, EventArgs e)
+        {
+            listBoxDevices.Items.Clear();
+
+            string[] result = GetARPResult();
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = result[i].Trim();
+            }
+
+            listBoxDevices.Items.AddRange(result);
+        }
+
+        private static string[] GetARPResult()
+        {
+            Process p = null;
+            string output = string.Empty;
+
+            try
+            {
+                p = Process.Start(new ProcessStartInfo(
+                    @"C:\Windows\SysWOW64\ARP.EXE", "-a")
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                });
+
+                output = p.StandardOutput.ReadToEnd();
+
+                p.Close();
+            }
+            catch
+            {
+                
+            }
+
+            finally
+            {
+                if (p != null)
+                {
+                    p.Close();
+                }
+            }
+            string[] stringSeparators = new string[] { "\n" };
+
+            return output.Split(
+                stringSeparators, StringSplitOptions.None); ;
+        }
+
+        private void listBoxDevices_SelectedIndexChanged(
+            object sender, EventArgs e)
+        {
+            try
+            {
+                string ip = Regex.Match(
+                    listBoxDevices.Text, regexIp).Value;
+                string mac = Regex.Match(
+                    listBoxDevices.Text, regexMac).Value;
+
+                tbIp.Text = ip;
+                tbMac.Text = mac;
+            }
+
+            catch
+            {
+                tbIp.Text = "NONE";
+                tbMac.Text = "NONE";
+            }
+        }
+    }
+}
